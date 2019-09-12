81e2c32eec0491b55467553f19a66e14320f2d88
Callil Capuozzo <callil.capuozzo@gmail.com>
2019-09-12 13:08:48 -0400

    Add motion & fix progress

--- www/pages/wallet.js
+++ www/pages/wallet.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import { transparentize } from 'polished'
